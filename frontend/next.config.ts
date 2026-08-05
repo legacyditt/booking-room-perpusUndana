737fd5090defe684c62c91b61ee844ecfb395bf2
Delano Manafe <legacyditt11@gmail.com>
2026-08-05 10:29:54 +0800

    chore: setup route redirect to admin dashboard and update gitignore

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/admin/overview",
+        permanent: false, // Gunakan false karena ini sementara sebelum ada auth
+      },
+    ];
+  },
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [

9a9b746722289cbdb85bb50d5174628975867b13
Anthasss <anthasprog@gmail.com>
2026-08-26 15:03:29 +0800

    jaja

--- backend/src/types/express.d.ts
+++ backend/src/types/express.d.ts
@@ -1,8 +1,12 @@
-import "express-serve-static-core";
+import 'express';
 
-declare module "express-serve-static-core" {
-  interface Request {
-    userId?: string;
-    userRole?: string;
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+      userRole?: string;
+    }
   }
 }
+
+export {};
